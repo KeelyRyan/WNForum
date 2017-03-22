b7399af373ec8a6db1a783d236e66c7c9036b34f
KeelyRyan <keelyannryan@gmail.com>
2017-03-22 12:41:56 +0000

    commit

---
+++ views/header.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="A short description." />
+    <meta name="keywords" content="write now, writing, forum, fyp" />
+    <meta name="author" content="Keely Ryan">
+    <title>WriteNow Forum</title>
+    <link rel="icon" href="images/title.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div id="container fliuid">
+		<div class="row">
+      <!--Social media -->
+			<div class="col-sm-9">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="index.php">WriteNow!</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">WriteNow Forum</a></li>
+              <li><a href="#">Topics</a></li>
+              <li><a href="#">Categories</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+        </nav>
+      </div>
+      <div class="col-sm-3">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+              <a><?php
+              if($_SESSION['loggedin'])
+              {
+                echo 'Hello ' . $_SESSION['user_name'] . '. Not you? <a href="signout.php">Sign out</a>';
+              }
+              else
+              {
+                echo '<a href="signin.php">Sign in</a> or <a href="create_account.php">Create an account</a>.';
+              }
+              ?></a>
+            </div>
+        </nav>
+      </div>
+    </div>
+<div id="content">
