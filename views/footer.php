6a40803f0c3516268e4213ac9b8e54b3c8927ef4
KeelyRyan <keelyannryan@gmail.com>
2017-03-22 12:42:06 +0000

    footer

---
+++ views/footer.php
@@ -0,0 +1,14 @@
+</div>
+<div id="footer">
+        <div class="row">
+        <div class="col-sm-12 text-center">
+          <nav class="navbar navbar-default"
+        <p>&#169; Keely Anne Ryan</p>
+           <p>K00223358</p>
+     </nav>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
