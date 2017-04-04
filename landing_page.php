831d9922d21ae8034510d7f85fa75102fb6a75b5
KeelyRyan <keelyannryan@gmail.com>
2017-04-04 14:41:44 +0100

    Forum finished
    Beginning index

--- landing_page.php
+++ landing_page.php
@@ -35,7 +35,41 @@ else
                     echo '<h3><a href="category.php?id=' . $row['cat_id'] . '">' . $row['cat_name'] . '</a><h3>'. $row['cat_description'];
                 echo '</td>';
                 echo '<td class="rightpart">';
-                            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
+    //fetch last topic for each cat
+                        $topicsql = "SELECT
+                                        topic_id,
+                                        topic_subject,
+                                        topic_date,
+                                        topic_cat_id
+                                    FROM
+                                        topics
+                                    WHERE
+                                        topic_cat_id = " . $row['cat_id'] . "
+                                    ORDER BY
+                                        topic_date
+                                    DESC
+                                    LIMIT
+                                        1";
+
+                        $topicsresult = mysqli_query($conn, $topicsql);
+
+                        if(!$topicsresult)
+                        {
+                            echo 'Last topic could not be displayed.';
+                        }
+                        else
+                        {
+                            if(mysqli_num_rows($topicsresult) == 0)
+                            {
+                                echo 'no topics';
+                            }
+                            else
+                            {
+                                while($topicrow = mysqli_fetch_assoc($topicsresult))
+                                echo '<a href="topic.php?id=' . $topicrow['topic_id'] . '">' . $topicrow['topic_subject'] . '</a> <br> on ' . date('d-m-Y', strtotime($topicrow['topic_date']));
+                            }
+                        }
+
                 echo '</td>';
             echo '</tr>';
         }
